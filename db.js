5dc4a6f1453944830859485960477f06955c6a59
Gabriela Sanchez <gaby.peta@gmail.com>
2020-07-23 14:30:31 +0200

    Add heroku

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
+require('./extractHerokuDatabaseEnvVars')();
 require('dotenv').config();
 
 const argon2 = require('argon2');
 const postgres = require('postgres');
-const sql = postgres();
+
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
 
 export async function getPlaces() {
   const places = await sql`
